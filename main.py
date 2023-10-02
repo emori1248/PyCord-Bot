133ddce418da60381609a9a800a06226fe87e80f
Ethan Mori <egmori2@gmail.com>
2023-10-01 21:36:01 -0400

    attempt to request from vercel api

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import discord
 from discord.ext import commands
 
+import requests
+# import re
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,7 +18,10 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    # escaped_query = re.escape("https://bot-webapp.vercel.app/api/trpc/rules.getAll?batch=1&input=%7B%220%22%3A%7B%22json%22%3A%7B%22guild_id%22%3A%22846599213440696360%22%7D%7D%7D")
+    response = requests.get("https://bot-webapp.vercel.app/api/trpc/rules.getAll?batch=1&input=%7B%220%22%3A%7B%22json%22%3A%7B%22guild_id%22%3A%22846599213440696360%22%7D%7D%7D")
+    print(response)
+    await ctx.send(response)
 
 @bot.command()
 async def hello(ctx):
